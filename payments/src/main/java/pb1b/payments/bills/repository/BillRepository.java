1443d876d2f70715d5a5fdd97c1cd690e3c1014b
Lokenath123 <110030320+Lokenath123@users.noreply.github.com>
2022-07-26 23:17:19 +0530

    Update BillRepository.java

--- payments/src/main/java/pb1b/payments/bills/repository/BillRepository.java
+++ payments/src/main/java/pb1b/payments/bills/repository/BillRepository.java
@@ -3,7 +3,11 @@ package pb1b.payments.bills.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import pb1b.payments.bills.dto.Bills;
-
+/**
+ * 
+ * @author LOKENATH BASU
+ *
+ */
 
 public interface BillRepository extends JpaRepository<Bills,String> {
 
