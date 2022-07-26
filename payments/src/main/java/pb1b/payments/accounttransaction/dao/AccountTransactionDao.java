fa8bc82e70eee4270a1a4ebe52d8e03d01ddb899
srishti <srishti11bajaj@gmail.com>
2022-07-26 16:50:24 +0530

    Sarthak-Commit

--- payments/src/main/java/pb1b/payments/accounttransaction/dao/AccountTransactionDao.java
+++ payments/src/main/java/pb1b/payments/accounttransaction/dao/AccountTransactionDao.java
@@ -6,6 +6,8 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import pb1b.payments.accounttransaction.repository.AccountTransactionRepository;
+
 
 @Repository
 public class AccountTransactionDao {
