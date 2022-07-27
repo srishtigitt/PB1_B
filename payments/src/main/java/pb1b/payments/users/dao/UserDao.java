17dc1a0e0ecff759a5503b2ed5da085e7d13b7db
Venuu Maadhav V R <44134958+VenuuMaadhav@users.noreply.github.com>
2022-07-27 09:22:01 +0530

    Update UserDao.java
    Added comments - Venuu Maadhav

--- payments/src/main/java/pb1b/payments/users/dao/UserDao.java
+++ payments/src/main/java/pb1b/payments/users/dao/UserDao.java
@@ -9,25 +9,46 @@ import org.springframework.stereotype.Repository;
 import pb1b.payments.users.dto.Users;
 import pb1b.payments.users.repository.UserRepository;
 
+/*
+* UserDao class is used to perform CRUD operations.
+* 
+* @author Venuu Maadhav V R
+*/
 
 @Repository
 public class UserDao {
 
 	@Autowired
 	private UserRepository userRepository;
-
+	/*
+	* 
+	* saveUser is used to save the User
+	* 
+	*/
 	public Users saveUser(Users user) {
 		return userRepository.save(user);
 	}
-
+	/*
+	* 
+	* updateUser is used to update the User
+	* 
+	*/
 	public Users updateUser(Users user) {
 		return userRepository.save(user);
 	}
-
+	/*
+	* 
+	* getAll is used to retrieve all the Users
+	* 
+	*/
 	public List<Users> getAll() {
 		return userRepository.findAll();
 	}
-
+	/*
+	* 
+	* getUserById is used to retrieve the User by their id
+	* 
+	*/
 	public Users getUserById(String id) {
 		Optional<Users> optional = userRepository.findById(id);
 		if (optional.isPresent()) {
@@ -35,7 +56,11 @@ public class UserDao {
 		}
 		return null;
 	}
-
+	/*
+	* 
+	* deleteUserById is used to delete the User by their id
+	* 
+	*/
 	public boolean deleteUserById(String id) {
 		Users student = getUserById(id);
 		if (student != null) {
